78d4dd8ec9cf4edb49f7aacee8d44fda26eaa1ad
Red <crabmarket56@gmail.com>
2023-01-26 22:20:02 +0900

    Draft tweet streaming logic

--- tweets.py
+++ tweets.py
@@ -9,6 +9,21 @@ api_key = config['twitter']['api_key']
 api_key_secret = config['twitter']['api_key_secret']
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
+bearer_token = config['twitter']['bearer_token']
 
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuth2AppHandler(api_key, api_key_secret)
+api = tweepy.API(auth)
+
+class Listener(tweepy.StreamingClient):
+
+  tweets = []
+  limit = 10
+  def on_status(self, status):
+    while limit > 0:
+      print(status.text)
+      limit -= 1
+
+stream_tweet = Listener(api_key, api_key_secret, access_token, access_token_secret)
+
+keywords = ['2022']
+stream_tweet.filter(track=keywords)
