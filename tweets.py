2175a3c420a9b387eb61802276d77a7523d11256
Red <crabmarket56@gmail.com>
2023-01-26 21:58:41 +0900

    Add API authentication

--- tweets.py
+++ tweets.py
@@ -8,4 +8,7 @@ config.read('config.ini')
 api_key = config['twitter']['api_key']
 api_key_secret = config['twitter']['api_key_secret']
 access_token = config['twitter']['access_token']
-access_token = config['twitter']['access_token_secret']
+access_token_secret = config['twitter']['access_token_secret']
+
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
