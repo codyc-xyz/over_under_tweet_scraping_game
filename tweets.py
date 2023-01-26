0af03a3d537e6e37cdabd008aa1b4a63aee708fb
Red <crabmarket56@gmail.com>
2023-01-26 19:33:34 +0900

    Migrate concept to tweepy

--- tweets.py
+++ tweets.py
@@ -1,2 +1,11 @@
-import snscrape.modules.twitter as twitter
+import tweepy
+import configparser
 import pandas as pd
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+access_token = config['twitter']['access_token']
+access_token = config['twitter']['access_token_secret']
