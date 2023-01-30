e0d92817f731414b77926a5740ebd678389f7174
Red <crabmarket56@gmail.com>
2023-01-30 15:34:42 +0900

    Add data sorting

--- tweets.py
+++ tweets.py
@@ -41,3 +41,14 @@ class TweetPrinter(tweepy.StreamingClient):
 printer = TweetPrinter(bearer_token)
 printer.sample()
 printer.disconnect()
+
+df = pd.read_csv('tweets.csv')
+hashtags = []
+for tweet in df['Tweet']:
+    hashtags_in_tweet = [i for i in tweet.split() if i.startswith('#')]
+    hashtags.extend(hashtags_in_tweet)
+
+hashtag_counts = pd.Series(hashtags).value_counts()
+sorted_hashtags = hashtag_counts.sort_values(ascending=False)
+top_1000_hashtags = sorted_hashtags[:1000]
+print(top_1000_hashtags)
