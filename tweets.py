210afcd8eec9e92b622c1443be61846ad7e8ac4f
Red <crabmarket56@gmail.com>
2023-01-30 15:49:45 +0900

    Modularize code

--- tweets.py
+++ tweets.py
@@ -2,53 +2,51 @@ import tweepy
 import configparser
 import pandas as pd
 import json
-import time
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+def read_config(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return {
+        'api_key': config['twitter']['api_key'],
+        'api_key_secret': config['twitter']['api_key_secret'],
+        'access_token': config['twitter']['access_token'],
+        'access_token_secret': config['twitter']['access_token_secret'],
+        'bearer_token': config['twitter']['bearer_token']
+    }
 
-api_key = config['twitter']['api_key']
-api_key_secret = config['twitter']['api_key_secret']
-access_token = config['twitter']['access_token']
-access_token_secret = config['twitter']['access_token_secret']
-bearer_token = config['twitter']['bearer_token']
+def stream_tweets(bearer_token):
+    class TweetPrinter(tweepy.StreamingClient):
+        def __init__(self, bearer_token):
+            super().__init__(bearer_token)
+            self.columns = ['Tweet']
+            self.data = []
 
-streaming_client = tweepy.StreamingClient(bearer_token)
-auth = tweepy.OAuth2AppHandler(api_key, api_key_secret)
-api = tweepy.API(auth)
+        def on_data(self, data):
+            json_data = json.loads(data)
+            if '#' in json_data['data']['text']:
+                self.data.append((json_data['data']['text']))
+
+        def on_error(self, status):
+            print(status)        
+
+        def on_disconnect(self):
+            df = pd.DataFrame(self.data, columns=self.columns)
+            df.to_csv('tweets.csv', index=False)
+            return super().on_disconnect()
+
+    printer = TweetPrinter(bearer_token)
+    printer.sample()
+
+def analyze_tweets(file):
+    df = pd.read_csv(file)
+    hashtags = []
+    for tweet in df['Tweet']:
+        hashtags_in_tweet = [i for i in tweet.split() if i.startswith('#')]
+        hashtags.extend(hashtags_in_tweet)
+
+    hashtag_counts = pd.Series(hashtags).value_counts()
+    sorted_hashtags = hashtag_counts.sort_values(ascending=False)
+    top_1000_hashtags = sorted_hashtags[:1000]
+    print(top_1000_hashtags)
 
 
-class TweetPrinter(tweepy.StreamingClient):
-    def __init__(self, bearer_token):
-        super().__init__(bearer_token)
-        self.columns = ['Tweet']
-        self.data = []
-    
-    def on_data(self, data):
-        json_data = json.loads(data)
-        if '#' in json_data['data']['text']:
-            self.data.append((json_data['data']['text']))
-
-    def on_error(self, status):
-        print(status)        
-
-    def on_disconnect(self):
-        df = pd.DataFrame(printer.data, columns=printer.columns)
-        df.to_csv('tweets.csv', index=False)
-        return super().on_disconnect()
-
-
-printer = TweetPrinter(bearer_token)
-printer.sample()
-printer.disconnect()
-
-df = pd.read_csv('tweets.csv')
-hashtags = []
-for tweet in df['Tweet']:
-    hashtags_in_tweet = [i for i in tweet.split() if i.startswith('#')]
-    hashtags.extend(hashtags_in_tweet)
-
-hashtag_counts = pd.Series(hashtags).value_counts()
-sorted_hashtags = hashtag_counts.sort_values(ascending=False)
-top_1000_hashtags = sorted_hashtags[:1000]
-print(top_1000_hashtags)
